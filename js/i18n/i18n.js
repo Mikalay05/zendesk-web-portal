de9ed4c760529a7e804957014b3f39639732b9aa
Mikalay05 <130233817+Mikalay05@users.noreply.github.com>
2024-06-18 09:43:55 +0300

    fix: i18n

--- js/i18n/i18n.js
+++ js/i18n/i18n.js
@@ -1,33 +1,60 @@
+let langData = {};
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('перевод загрузка')
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("перевод загрузка");
+  fetch("../json/localization.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Ой, ошибка в fetch: " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((jsonData) => {
+      langData = jsonData;
+      console.log(langData);
 
-    const lang = localStorage.getItem('language') || 'en';
-    translatePage(lang);
-    document.querySelector('[data-header-translate="language"]').addEventListener('click', toggleLanguage);
-    document.querySelector('.lng-line.in-burger').addEventListener('click', toggleLanguage);
-})
+      // После загрузки данных из JSON вызываем translatePage
+      const lang = localStorage.getItem("language") || "en";
+      translatePage(lang);
+    })
+    .catch((error) => console.error("Ошибка при исполнении запроса: ", error));
+
+  document
+    .querySelector('[data-header-translate="language"]')
+    .addEventListener("click", toggleLanguage);
+  document
+    .querySelector(".lng-line.in-burger")
+    .addEventListener("click", toggleLanguage);
+});
 
 function translatePage(lang) {
-    console.log('перевод старницы')
-
-    translateHeader(lang);
-    translateFooter(lang);
-    translateMain(lang);
-
+  console.log("перевод страницы");
+  translateHeader(lang);
+  translateFooter(lang);
+  translateMain(lang);
 }
+
 function toggleLanguage() {
-    console.log('смена языка')
-
-    const currentLang = localStorage.getItem('language') || 'en';
-    const newLang = currentLang=='en'? 'ru': 'en';
-    localStorage.setItem('language',newLang);
-    translatePage(newLang);
+  console.log("смена языка");
+  const currentLang = localStorage.getItem("language") || "en";
+  const newLang = currentLang === "en" ? "ru" : "en";
+  localStorage.setItem("language", newLang);
+  translatePage(newLang);
 }
+
 function translateMain(lang) {
-    const elements = document.querySelectorAll('[data-translate]');
-    elements.forEach(element => {
-        const key = element.getAttribute('data-translate');
-        element.textContent = translationsMain[lang][key];
-    });
+  console.log("перевод main");
+  const elements = document.querySelectorAll("[data-translate]");
+  console.log(elements);
+  elements.forEach((element) => {
+    const key = element.getAttribute("data-translate");
+    console.log(element);
+    console.log(langData);
+
+    if (langData[key] && langData[key][lang]) {
+      element.textContent = langData[key][lang];
+    } else {
+      console.warn(`Перевод для ключа "${key}" на языке "${lang}" не найден.`);
+    }
+  });
 }
